5f637b9de18c163ccb2574c1f3552794531197be
mikifriki <mihkel.liblikman@gmail.com>
2020-04-13 18:10:38 +0300

    Made Favorites into a functional component

--- src/Component/Favorites.js
+++ src/Component/Favorites.js
@@ -1,41 +1,38 @@
-/* eslint-disable */
 import React from "react";
 import "../CSS/Favorite.css";
 import {Button, Header, Image, List} from 'semantic-ui-react';
 import arrow from "../Image/arrows.png"
 
-export default class Favorites extends React.Component {
+const Favorites = ({onFavorite, onRemoveFavorite, visible}) => {
+	console.log(onFavorite, "Favorites");
 
-	//Creating a table to be shown in the FavoriteWindow div
-	createTable = () => {
+	const createTable = () => {
 		let rows = [];
 		for (let i = 0; i < localStorage.length; i++) {
-			let favorites = localStorage.key(i);
 			rows.push(
 				<List.Item key={i}>
-					<a id="FavoriteLink" onClick={this.props.onFavorite}>{favorites}</a>
-					<Button negative size='mini' onClick={() => this.props.onRemoveFavorite(favorites)}>Delete</Button>
+					<a id="FavoriteLink" onClick={onFavorite}>{localStorage.key(i)}</a>
+					<Button negative size='mini' onClick={() => onRemoveFavorite(localStorage.key(i))}>Delete</Button>
 				</List.Item>
 			)
 		}
 		return rows;
 	};
 
-	//Setting up the structure for the favorite window
-	render() {
-		if (!this.props.visible) return <></>;
-		return (
-			<div className="FavoriteWindow">
-				<Header id="FavoriteTitle">
-					Favorites
-				</Header>
-				<hr className="brace"/>
-				<List id="FavoriteText">
-					{this.createTable()}
-				</List>
-				<Image className="arrow bounce" src={arrow} size="small" href="#"/>
-			</div>
-		)
-	}
-}
+	if (!visible) return <></>;
+	return (
+		<div className="FavoriteWindow">
+			<Header id="FavoriteTitle">
+				Favorites
+			</Header>
+			<hr className="brace"/>
+			<List id="FavoriteText">
+				{createTable()}
+			</List>
+			<Image className="arrow bounce" src={arrow} size="small" href="#"/>
+		</div>
+	)
+};
+
+export default Favorites;
 
